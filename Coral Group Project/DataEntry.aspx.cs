71d31f7a0c2fa4c139db6373bc4b8489947d1aac
naanmohammed <naanahmed24@gmail.com>
2024-03-03 13:07:26 +0300

    Add logic to set data for data entry page

---
+++ Coral Group Project/DataEntry.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DataEntry : System.Web.UI.Page
+{
+    protected void SubmitData(object sender, EventArgs e)
+    {
+        string vehicleType = ddlVehicleType.SelectedValue;
+        double distance = Convert.ToDouble(txtDistance.Text);
+        string fuelType = ddlFuelType.SelectedValue;
+        double fuelEfficiency = Convert.ToDouble(txtFuelEfficiency.Text);
+        string energySource = ddlEnergySource.SelectedValue;
+        double electricityUsage = Convert.ToDouble(txtElectricityUsage.Text);
+
+    }
+}
